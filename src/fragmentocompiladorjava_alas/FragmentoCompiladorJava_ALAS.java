bb48a0b67cd938c6a7b925ef43f10d40cb03d7ab
JLCG17 <juluanis17@gmail.com>
2014-09-14 21:00:10 -0300

    Update FragmentoCompiladorJava_ALAS.java

--- src/fragmentocompiladorjava_alas/FragmentoCompiladorJava_ALAS.java
+++ src/fragmentocompiladorjava_alas/FragmentoCompiladorJava_ALAS.java
@@ -17,6 +17,7 @@ public class FragmentoCompiladorJava_ALAS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Wololoooooo...
     }
     
 }
